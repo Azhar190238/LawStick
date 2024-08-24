c5e5e948844f8d1c9f66c93e8d92554099a62fb6
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-25 01:58:36 +0600

    Alhamdulillah updated

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-[1320px] mx-auto">
    <h1 className="text-7xl">ALhamdulilah  </h1>
     </div>
   );
