421000bf9ab4cb3143e9000e84a512644f5b0ad2
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-26 22:36:53 +0600

    file updated

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import AboutUs from "@/components/AboutUs";
 import Blog from "@/components/Blog";
 import Experience from "@/components/Experience";
-import Expert from "@/components/Expert";
 import Hero from "@/components/Hero";
 import OurService from "@/components/OurService";
 import Team from "@/components/Team/Team";
 import WhatWeUs from "@/components/WhatWeUs";
+import Expert from "@/components/Expert/Expert"
 
 
 export default function Home() {
