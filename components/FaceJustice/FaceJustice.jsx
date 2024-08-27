7705bbf6158eb09d6057369c87fa46d1a3f8e983
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-27 21:02:17 +0600

    Alhamdulillah updated

--- components/FaceJustice/FaceJustice.jsx
+++ components/FaceJustice/FaceJustice.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 
 import { useRef, useState } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
