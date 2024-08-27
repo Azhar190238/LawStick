a5d387fab19b6caced29956d3b72691e87c519b7
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-27 20:44:35 +0600

    Alhamdulillah updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -172,9 +172,7 @@
 //             <div className="navbar-center hidden lg:flex">
 //                 <ul className="flex flex-row px-1 space-x-[30px] text-[16px]">
 //                     {links.map(link => (
-//                         <li key={link.key}>
-//                             {link.submenu ? (
-//                                 <div className="dropdown inline-block relative">
+
 
 
 
