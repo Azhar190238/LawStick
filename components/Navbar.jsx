5eeb3d2f54df138d2af33191674ceb1fce9ce9ec
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-27 20:46:52 +0600

    Alhamdulillah updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -119,9 +119,7 @@
 //             <div className="navbar-start">
 //                 <div className="dropdown">
 //                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-//                         <svg
-//                             xmlns="http://www.w3.org/2000/svg"
-//                             className="h-5 w-5"
+
 
 
 
