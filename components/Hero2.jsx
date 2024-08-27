bd857206291f5a2d7aa4c3a3fe58d7250ddcaafb
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-27 21:01:54 +0600

    Alhamdulillah updated

--- components/Hero2.jsx
+++ components/Hero2.jsx
@@ -1,18 +1,3 @@
-// // import Image from 'next/image';
-// // import React from 'react';
-
-// // const Hero2 = () => {
-
-
-
-
-
-
-
-
-
-
-
 
 import Image from 'next/image';
 import React from 'react';
