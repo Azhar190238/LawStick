42da52b903b50b32b8b4ec7d01e4edff21179fd1
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-27 20:42:38 +0600

    Alhamdulillah updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -209,12 +209,7 @@
 //                 <button className="px-8 py-4 border-2 border-[#B68C5A] text-[#1D1C22] text-xl rounded-[4px] hover:bg-[#B68C5A] hover:text-white transition-colors duration-300">
 //                     Get Appointment
 //                 </button>
-//             </div>
-//         </div>
-//     );
-// };
 
-// export default Navbar;
 
 
 
