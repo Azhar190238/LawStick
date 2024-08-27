f1648d84342cfc494cf619f7fadfd3ca2b576e85
MD AZHARUDDIN <azhar73397@gmail.com>
2024-08-27 20:44:47 +0600

    Alhamdulillah updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -169,9 +169,7 @@
 //                 </div>
 //                 <Image src='/logo.png' height={46} width={157} alt="logo image"></Image>
 //             </div>
-//             <div className="navbar-center hidden lg:flex">
-//                 <ul className="flex flex-row px-1 space-x-[30px] text-[16px]">
-//                     {links.map(link => (
+
 
 
 
